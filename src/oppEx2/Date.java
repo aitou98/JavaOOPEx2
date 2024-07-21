d8ca309478ab926a36e59623d6535e295a56a622
yassine aitoukallal <yassine.aitoukallal@gmail.com>
2024-07-21 20:03:58 +0100

    first commit

--- src/oppEx2/Date.java
+++ src/oppEx2/Date.java
@@ -1,5 +1,51 @@
 package oppEx2;
 
 public class Date {
+	
+	private int year,month,day;
+	
+	public Date(int year , int month , int day) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+
+	public int getYear() {
+		return this.year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getMonth() {
+		return this.month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getDay() {
+		return this.day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%d/%d/%d", month, day, year);
+	}
+	
+	
+	public void setDate(int year , int month , int day) {
+		this.year = year;
+	      this.month = month;
+	      this.day = day;
+	}
+	
+	
 
 }
--- src/oppEx2/Main.java
+++ src/oppEx2/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		Date d1 = new Date(2025, 1, 1);
+	      System.out.println(d1);  
+	      
+	     
+
 
 	}
 
